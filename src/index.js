b2869f30241f44f3e2be16a3966624613cfc8e79
Tr1kste <trikster.seo@gmail.com>
2022-01-11 01:36:27 +0300

    feat: final function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(num) {
+    const newStr = String(num)
+    let revstr = '';
+    for (let i = newStr.length - 1; i >= 0; i--) {
+        revstr = revstr + newStr[i];
+    }
+    return revstr.replace(/\D/g, '');
 }
